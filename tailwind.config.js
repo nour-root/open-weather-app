8e4e94ad8586d2bad57b48aac99793899e1dbe27
Nour Mahmoud <nour.m.mahmod@gmail.com>
2024-12-19 15:40:04 +0300

    update html template

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sky-blue": "#43AEFC",
+      },
+    },
   },
   plugins: [],
 };
